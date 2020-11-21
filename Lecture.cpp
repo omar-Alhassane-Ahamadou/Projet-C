13aef606997493dd4a06ba5c6fcd0450ef5f56fc
Omar ALHASSANEAHAMADOU <omaralhassane11@gmail.com>
2020-11-21 14:24:20 +0100

    Test du code de la lecture de AND2

---
+++ Lecture.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+  fstream Lecture_file("AND2.gv", ios::in);
+  string line ;
+  while(getline(Lecture_file, line)){
+    cout<<line<<endl;
+  }
+  cout<<"Fin de la Lecture des données du fichier"<<endl;
+  Lecture_file.close();
+  return 0;
+}
